5c630d66fa6666868c52d8e44096a3c3f41a6d7d
Avishekshaw35 <shawabhishek513@gmail.com>
2022-07-19 17:39:47 +0530

    Initial commit

---
+++ Multiplier.sim/sim_1/behav/xsim/xsim.dir/Multiplier_tb_behav/obj/xsim_1.c
@@ -0,0 +1,152 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_39(char*, char *);
+extern void execute_40(char*, char *);
+extern void execute_118(char*, char *);
+extern void execute_119(char*, char *);
+extern void execute_120(char*, char *);
+extern void execute_36(char*, char *);
+extern void execute_37(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_45(char*, char *);
+extern void execute_46(char*, char *);
+extern void execute_47(char*, char *);
+extern void execute_48(char*, char *);
+extern void execute_100(char*, char *);
+extern void execute_101(char*, char *);
+extern void execute_102(char*, char *);
+extern void execute_103(char*, char *);
+extern void execute_104(char*, char *);
+extern void execute_105(char*, char *);
+extern void execute_106(char*, char *);
+extern void execute_107(char*, char *);
+extern void execute_108(char*, char *);
+extern void execute_109(char*, char *);
+extern void execute_110(char*, char *);
+extern void execute_111(char*, char *);
+extern void execute_112(char*, char *);
+extern void execute_113(char*, char *);
+extern void execute_114(char*, char *);
+extern void execute_115(char*, char *);
+extern void execute_116(char*, char *);
+extern void execute_117(char*, char *);
+extern void execute_49(char*, char *);
+extern void execute_50(char*, char *);
+extern void execute_55(char*, char *);
+extern void execute_42(char*, char *);
+extern void execute_43(char*, char *);
+extern void execute_44(char*, char *);
+extern void execute_121(char*, char *);
+extern void execute_122(char*, char *);
+extern void execute_123(char*, char *);
+extern void execute_124(char*, char *);
+extern void execute_125(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void transaction_64(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_65(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_66(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_67(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_69(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_70(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_71(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_72(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_86(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_87(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_88(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_89(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_91(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_92(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_93(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_94(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[58] = {(funcp)execute_39, (funcp)execute_40, (funcp)execute_118, (funcp)execute_119, (funcp)execute_120, (funcp)execute_36, (funcp)execute_37, (funcp)execute_38, (funcp)execute_45, (funcp)execute_46, (funcp)execute_47, (funcp)execute_48, (funcp)execute_100, (funcp)execute_101, (funcp)execute_102, (funcp)execute_103, (funcp)execute_104, (funcp)execute_105, (funcp)execute_106, (funcp)execute_107, (funcp)execute_108, (funcp)execute_109, (funcp)execute_110, (funcp)execute_111, (funcp)execute_112, (funcp)execute_113, (funcp)execute_114, (funcp)execute_115, (funcp)execute_116, (funcp)execute_117, (funcp)execute_49, (funcp)execute_50, (funcp)execute_55, (funcp)execute_42, (funcp)execute_43, (funcp)execute_44, (funcp)execute_121, (funcp)execute_122, (funcp)execute_123, (funcp)execute_124, (funcp)execute_125, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_64, (funcp)transaction_65, (funcp)transaction_66, (funcp)transaction_67, (funcp)transaction_69, (funcp)transaction_70, (funcp)transaction_71, (funcp)transaction_72, (funcp)transaction_86, (funcp)transaction_87, (funcp)transaction_88, (funcp)transaction_89, (funcp)transaction_91, (funcp)transaction_92, (funcp)transaction_93, (funcp)transaction_94};
+const int NumRelocateId= 58;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/Multiplier_tb_behav/xsim.reloc",  (void **)funcTab, 58);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/Multiplier_tb_behav/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+	iki_schedule_processes_at_time_zero(dp, "xsim.dir/Multiplier_tb_behav/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/Multiplier_tb_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/Multiplier_tb_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/Multiplier_tb_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
